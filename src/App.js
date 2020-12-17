1c4df656bfe9a8f3eed5ce759fcf5b0b2465f98d
Amit <amit.kumar.tiwary@sap.com>
2020-12-17 23:52:54 +0530

    initial checkin

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Carousel from './components/carousel.js'
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+        
+    //   </header>
+    //   <Carousel />
+    // </div>
+    
+    <div id="root"></div>
+  );
+}
+
+export default App;
---
+++ src/components/carousel.js
@@ -0,0 +1,196 @@
+// Carousel made using vanila JS and REACT js by Amit Kumar Tiwary
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/styles.scss';
+const slideWidth = 30;
+
+const _items = [
+    {
+        fashion: {
+            name: 'MNV Tshirt',
+            image: 'https://imgur.com/GIhgpqk.jpg',
+            price: "Rs 120",
+            category: "Tshirt"
+        }
+    },
+    {
+        fashion: {
+            name: 'Brand XXX Jeans',
+            image: 'https://imgur.com/9AoLPJK.jpg',
+            price: "Rs 440",
+            category: "Jeans"
+        }
+    },
+    {
+        fashion: {
+            name: 'MNB XXX Jeans',
+            image: 'https://imgur.com/9AoLPJK.jpg',
+            price: "Rs 120",
+            category: "Jeans"
+        }
+    },
+    {
+        fashion: {
+            name: 'BOOM XXX',
+            image: 'https://imgur.com/XbYp65k.jpg',
+            price: "Rs 780",
+            category: "Jacket"
+        }
+    },
+    {
+        fashion: {
+            name: 'Brand ABC SOCKS',
+            image: 'https://imgur.com/h3Ch7tK.jpg',
+            price: "Rs 70",
+            category: "socks"
+        }
+    }
+]
+
+const length = _items.length
+_items.push(..._items)
+
+
+const sleep = (ms = 0) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+const createItem = (position, idx, activeIdx) => {
+    const item = {
+        styles: {
+            transform: `translateX(${position * slideWidth}rem)`
+        },
+        fashion: _items[idx].fashion
+       
+    }
+
+    switch (position) {
+        case length - 1:
+        case length + 1:
+            item.styles = { ...item.styles, filter: 'grayscale(1)',height: '25rem',top: '2em'  }
+            break
+        case length:
+            break
+        default:
+            item.styles = { ...item.styles, opacity: 0}
+            break
+
+    }
+
+    return item
+}
+const Carouselheader =()=>{
+    return (
+        <div className="header">
+            <p>Paypal Assignments Test</p>
+            <p className="designed">Designed and developed by Amit Kumar Tiwary</p>
+            <p className="problemStatement">Carousel containing list of product items using vanilla JavaScript and React JS</p>
+        </div>
+    )
+}
+
+const CarouselSlideItem = ({ pos, idx, activeIdx }) => {
+    const item = createItem(pos, idx, activeIdx)
+
+    return (
+        <li className='carousel__slide-item' style={item.styles}>
+            <div className='carousel__slide-item-img-link'>
+                <img src={item.fashion.image} alt={item.fashion.title} />
+            </div>
+            <div className='carousel-slide-item__body'>
+                <h3>{item.fashion.name}</h3>
+                <p>Price : <span className="price">{item.fashion.price}</span></p>
+                <p className="category">{item.fashion.category}</p>
+            </div>
+        </li>
+    )
+}
+
+const keys = Array.from(Array(_items.length).keys())
+
+const Carousel = () => {
+    const [items, setItems] = React.useState(keys)
+    const [isTicking, setIsTicking] = React.useState(false)
+    const [activeIdx, setActiveIdx] = React.useState(0)
+    const bigLength = items.length
+
+    const prevClick = (jump = 1) => {
+        if (!isTicking) {
+            setIsTicking(true)
+            setItems(prev => {
+                return prev.map((_, i) => prev[(i + jump) % bigLength])
+            })
+        }
+    }
+
+    const nextClick = (jump = 1) => {
+        if (!isTicking) {
+            setIsTicking(true)
+            setItems(prev => {
+                return prev.map(
+                    (_, i) => prev[(i - jump + bigLength) % bigLength]
+                )
+            })
+        }
+    }
+
+    const handleDotClick = idx => {
+        if (idx < activeIdx) prevClick(activeIdx - idx)
+        if (idx > activeIdx) nextClick(idx - activeIdx)
+    }
+
+    React.useEffect(() => {
+        if (isTicking) sleep(300).then(() => setIsTicking(false))
+    }, [isTicking])
+
+    React.useEffect(() => {
+        setActiveIdx((length - (items[0] % length)) % length) // prettier-ignore
+    }, [items])
+
+    return (
+        <div>
+            <Carouselheader />
+        
+        <div className='carousel__wrap'>
+            <div className='carousel__inner'>
+                <button
+                    className='carousel__btn carousel__btn--prev'
+                    onClick={() => prevClick()}>
+                    <i className='carousel__btn-arrow carousel__btn-arrow--left' />
+                </button>
+                <div className='carousel__container'>
+                    <ul className='carousel__slide-list'>
+                        {items.map((pos, i) => (
+                            <CarouselSlideItem
+                                key={i}
+                                idx={i}
+                                pos={pos}
+                                activeIdx={activeIdx}
+                            />
+                        ))}
+                    </ul>
+                </div>
+                <button
+                    className='carousel__btn carousel__btn--next'
+                    onClick={() => nextClick()}>
+                    <i className='carousel__btn-arrow carousel__btn-arrow--right' />
+                </button>
+                <div className='carousel__dots'>
+                    {items.slice(0, length).map((pos, i) => (
+                        <button
+                            key={i}
+                            onClick={() => handleDotClick(i)}
+                            className={i === activeIdx ? 'dot active' : 'dot'}
+                        />
+                    ))}
+                </div>
+            </div>
+            </div>
+        
+        </div>
+        
+    )
+}
+// export default Carousel;
+
+ReactDOM.render(<Carousel />, document.getElementById("root"));
